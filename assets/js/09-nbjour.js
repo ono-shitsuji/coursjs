4b75969e755d9f0fb0c8d991fbb9cd0d3aa9a808
ono-shitsuji <fbernard.kylian@gmail.com>
2025-07-15 14:06:19 +0200

    cours upgrade

--- assets/js/09-nbjour.js
+++ assets/js/09-nbjour.js
@@ -52,4 +52,59 @@ if (mois === 2) { // si on a choisi février => chiffre 2
     document.write("Le mois numéro " + mois + " fait 31 jours.");
 } else {
     document.write("Votre choix " + mois + " n'est pas dans le calendrier.");
-}
+} 
+
+
+// cours js 
+
+console.log("toto");
+
+let fruits = ['pomme', 'banane', 'fraise'];
+console.log(fruits.length);
+
+let premier = fruits[0];
+console.log(premier);
+
+let couleurs = ["Chartreuse", "Crimson", "DarkGoldenRod", "DarkTurquoise", "DarkSalmon"];
+
+console.log(couleurs[0]);
+
+//Boucler sur un tableau
+for(let i = 0; i < couleurs.length; i++ ){
+    console.log(couleurs[i]);
+    document.write(`<p style="color: ${couleurs[i]} ">J'aime les licornes</p>`)
+};
+
+//Ajouter à la fin du tableau
+couleurs.push("DarkOrchid");
+
+//Supprimer le dernier élément du tableau
+couleurs.pop();
+
+//Supprimer le premier élément du tableau
+couleurs.shift();
+
+//Ajouter au début du tableau
+couleurs.unshift("HotPink");
+
+// Trouver l'index d'un élément dans le tableau
+let pos = couleurs.indexOf("Crimson");
+console.log(pos);
+
+//Supprimer un élément par son index
+couleurs.splice(pos,1);
+
+//Boucler sur un tableau
+for(let i = 0; i < couleurs.length; i++ ){
+    document.write(`<p style="color: ${couleurs[i]} ">J'aime les licornes</p>`)
+};
+
+
+let tableauFruits = ['banane', "pomme", 'fraise', "pasteque", "melon"];
+
+let indexTab = 2;
+let nombre = 3;
+
+let nouveauTableauFruits = tableauFruits.splice(indexTab, nombre);
+//indexTab = à partir de la position,
+//nombre définit le nombre d'éléments à supprimer
